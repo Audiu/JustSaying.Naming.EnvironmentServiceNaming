b741c4c14411721be6fdc8ad945bd3b44b9e6130
Mike Goodfellow <mdgoodfellow@gmail.com>
2024-02-02 09:51:44 +0000

    Remove unneeed confusion

--- src/JustSaying.Naming.EnvironmentServiceNaming/QueueSubscriptionBuilderExtensions.cs
+++ src/JustSaying.Naming.EnvironmentServiceNaming/QueueSubscriptionBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using JustSaying.Fluent;
+﻿using JustSaying.Fluent;
 using JustSaying.Models;
 
 namespace JustSaying.Naming.EnvironmentServiceNaming
@@ -8,37 +7,9 @@ namespace JustSaying.Naming.EnvironmentServiceNaming
     {
         public static QueueSubscriptionBuilder<T> ConfigurePointToPointQueue<T>(
             this QueueSubscriptionBuilder<T> builder,
-            string environment,
-            TimeSpan? visibilityTimeout = null,
-            TimeSpan? messageRetention = null,
-            TimeSpan? deliveryDelay = null,
-            int? retriesBeforeErrorQueue = null) where T : Message
+            string environment) where T : Message
         {
-            builder
-                .WithQueueName(EnvironmentServiceNamingStrategy.GetPointToPointQueueName<T>(environment))
-                .WithReadConfiguration(
-                    c =>
-                    {
-                        if (messageRetention.HasValue)
-                        {
-                            c.MessageRetention = (TimeSpan)messageRetention;   
-                        }
-
-                        if (visibilityTimeout.HasValue)
-                        {
-                            c.VisibilityTimeout = (TimeSpan)visibilityTimeout;   
-                        }
-
-                        if (deliveryDelay.HasValue)
-                        {
-                            c.DeliveryDelay = (TimeSpan)deliveryDelay;   
-                        }
-                        
-                        if (retriesBeforeErrorQueue.HasValue)
-                        {
-                            c.RetryCountBeforeSendingToErrorQueue = (int)retriesBeforeErrorQueue;   
-                        }
-                    });
+            builder.WithQueueName(EnvironmentServiceNamingStrategy.GetPointToPointQueueName<T>(environment));
 
             return builder;
         }
